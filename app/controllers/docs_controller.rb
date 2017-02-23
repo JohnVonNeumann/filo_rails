535873b653aaa059c0f10da9c60b45dabbbc99db
JohnVonNeumann <louiswillcock@gmail.com>
2017-02-23 20:15:14 +1100

    added a resources :docs line to the routes.rb file in order to cleanly mass add routing for all the basic index,update,new... actions we can use on our docs_controller, also edited the docs_controller file itself to add in all the new and relevant actions we wish to use later, in this, we also create a private section with two methods find_doc and doc_params for further expansion later

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,2 +1,32 @@
 class DocsController < ApplicationController
+
+  def index
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+
+    def find_doc
+    end
+
+    def doc_params
+    end
+
 end
